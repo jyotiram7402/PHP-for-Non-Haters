07ba1b1260d1b44701c4b4fe6102d9b45a60b232
Jyotiram Kamble <jkamble@southco.com>
2025-06-20 18:35:20 +0530

    PHP Hello World

---
+++ PHP-Hello-Word/index.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Hello, World!";
+?>
+
+/*
+Explanation of the Code:
+
+<?php   ?> - This indicates the start and end of a PHP script.
+
+echo "Hello, World!"; - The echo statement is used to output text to the browser.
+
+; - Every PHP statement ends with a semicolon. 
+*/
